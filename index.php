0ee06b89464182c2f62c102742838f97e47f4a2c
NicollyeCrist <128860826+NicollyeCrist@users.noreply.github.com>
2023-06-14 21:48:54 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<center>
+Olá, <?php
+  if(isset($_SESSION['nome'])==null) {
+     ?> visitante <br>
+     <a href="login.php">login</a>
+     <?php } else {
+        echo $_SESSION['nome'];?> <br>
+        <a href= "cadastrar.php">cadastrar</><br>
+        <a href= "logout.php">sair</a>
+        <?php } ?>
+     
+
+</center>
+</body>
+</html>
