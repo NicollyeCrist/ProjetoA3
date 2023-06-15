f0fd1bf02ac024c1ef0a638775c41833713502a3
NicollyeCrist <128860826+NicollyeCrist@users.noreply.github.com>
2023-06-15 00:04:30 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ompatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tela de login</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(45deg, cyan, yellow);
+        }
+        div{
+            background-color: rgba(0, 0, 0, 0.9);
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            padding: 80px; 
+            border-radius: 15px; 
+            color: white;
+        }
+        input{
+            padding: 15px;
+            border: none;
+            outline: none;
+            font-size: 15px;
+        }
+        button{
+            background-color: dodgerblue;
+            border: none;
+            padding: 15px;
+            width: 100%;
+            border-radius: 10px;
+            color: white;
+            font-size: 15px;
+            cursor: pointer;
+        }
+        button:hover{
+            background-color:deepskyblue;
+        }
+
+    </style>
 </head>
 <body>
+      <div>
       <center>
         <h1>LOGIN</h1>
         <form id="login" action="logado.php" method="POST">
-         Login: <input type="text" id="login" required><br>
-         Senha: <input type="password" id="senha" required><br><br>
-         <input type= "submit" id="entrar" value="Entrar">
-         
+         <input type="text" placeholder="Nome" id="login" required>
+         <br><br>
+         <input type="password" placeholder="Senha" id="senha" required>
+         <br><br>
+         <button>Entrar</button>
+    </div>
  </form>
 </center>
 </body>
