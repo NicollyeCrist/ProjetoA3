5550c139d9102aab9f74a5347edd8a65d223674c
NicollyeCrist <128860826+NicollyeCrist@users.noreply.github.com>
2023-06-16 09:31:31 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
-ompatible" content="IE=edge">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de login</title>
     <style>
@@ -41,7 +45,7 @@ ompatible" content="IE=edge">
 <body>
       <div>
       <center>
-        <h1>LOGIN</h1>
+        <h1>Login</h1>
         <form id="login" action="logado.php" method="POST">
          <input type="text" placeholder="Nome" id="login" required>
          <br><br>
