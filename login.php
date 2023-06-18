8a738c1b37c80ca92410f0653445be508fa25862
NicollyeCrist <128860826+NicollyeCrist@users.noreply.github.com>
2023-06-18 20:54:54 -0300

    Update login.php

--- login.php
+++ login.php
@@ -4,56 +4,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de login</title>
+    <title>Login</title>
     <style>
-        body{
-            font-family: Arial, Helvetica, sans-serif;
-            background-image: linear-gradient(45deg, cyan, black);
-        }
-        div{
-            background-color: rgba(0, 0, 0, 0.9);
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%,-50%);
-            padding: 80px; 
-            border-radius: 15px; 
-            color: white;
-        }
-        input{
-            padding: 15px;
-            border: none;
-            outline: none;
-            font-size: 15px;
-        }
-        button{
-            background-color: dodgerblue;
-            border: none;
-            padding: 15px;
-            width: 100%;
-            border-radius: 10px;
-            color: white;
-            font-size: 15px;
-            cursor: pointer;
-        }
-        button:hover{
-            background-color:deepskyblue;
+        body {
+            background-image: url('https://wallpaperaccess.com/full/5493520.jpg');
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            padding: 0;
         }
 
+        #login {
+            background-color: rgba(255, 255, 255, 0.7);
+            padding: 20px;
+            border-radius: 5px;
+        }
+
+        #login input[type="text"],
+        #login input[type="password"] {
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            margin-bottom: 10px;
+        }
+
+        #login input[type="submit"] {
+            padding: 10px 20px;
+            background-color: #4f0d15;
+            color: white;
+            border: none;
+            cursor: pointer;
+        }
+
+        #login input[type="submit"]:hover {
+            background-color: #8b0000;
+        }
     </style>
 </head>
 <body>
-      <div>
-      <center>
-        <h1>Login</h1>
-        <form id="login" action="logado.php" method="POST">
-         <input type="text" placeholder="Nome" id="login" required>
-         <br><br>
-         <input type="password" placeholder="Senha" id="senha" required>
-         <br><br>
-         <button>Entrar</button>
-    </div>
- </form>
-</center>
+    <center>
+        <div id="login">
+            <h1>Login</h1>
+            <form action="logado.php" method="POST">
+                Login: <input type="text" name="login" required><br>
+                Senha: <input type="password" name="senha" required><br><br>
+                <input type="submit" value="Entrar">
+            </form>
+        </div>
+    </center>
 </body>
 </html>
