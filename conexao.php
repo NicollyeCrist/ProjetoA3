569f6b9dbfdc976989af506d6958a91e8564c91e
NicollyeCrist <128860826+NicollyeCrist@users.noreply.github.com>
2023-06-18 20:52:34 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$conexao= mysqli_connect ('localhost','root', '', 'projetoa3', '3306');
-if (!$conexao){
-    die ("erro de conexão");
-} else {
-    echo "conectado";
+
+$conexao = mysqli_connect('localhost', 'root','', 'projetoa3', '3306');
+
+if(!$conexao){
+   die("erro de conexão");
 }
 ?>
